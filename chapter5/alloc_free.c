8cee178be133c4eb50fccea40add4d9702262c56
Sangram Barge <sangrambarge098@gmail.com>
2023-08-01 07:12:54 +0530

    barebones alloc

---
+++ chapter5/alloc_free.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define ALLOCSIZE 1000 /* alloc buffer size */
+#define NULL 0 /* error return */
+
+static char allocbuff[ALLOCSIZE]; /* alloc buffer of 1000 size */
+static char *allocp = allocbuff; /* alloc pointer */
+
+void free(char *p);
+char *alloc(int n);
+
+char *alloc(int n) {
+  if (allocp + n <= allocbuff + ALLOCSIZE) {
+    allocp += n;
+    return (allocp - n);
+  }
+  return NULL;
+}
+
+void free(char *p) {
+  if (p >= allocbuff && p <= allocbuff + ALLOCSIZE) allocp = p;
+}
