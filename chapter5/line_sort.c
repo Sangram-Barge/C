92374170e772c2b61e0712168697598902e41911
Sangram Barge <sangrambarge098@gmail.com>
2023-08-02 22:10:57 +0530

    readlines function refactored

--- chapter5/line_sort.c
+++ chapter5/line_sort.c
@@ -15,17 +15,16 @@ int main(void) {
 }
 
 int readlines(char *lineptr[], int maxlines) {
-  int count = 0, len;
-  char line[MAXLEN], *p;
-  while ((len = get_line(line, MAXLEN)) > 0)
-      if (count > maxlines) return -1;
-      else if ((p = alloc(len)) == 0) return -1;
-      else {
-        line[len - 1] = '\0';
-        strcpy(p, line);
-        lineptr[count++] = p;
-      }
-  return count;
+  int count = 0, len = 0;
+  char *p, line[MAXLEN];
+  while ((len = get_line(line, MAXLEN)) != 0)
+    if (count >= LINE || (p = alloc(len)) == 0) return -1;
+    else {
+      line[len - 1] = '\0';
+      strcpy(p, line);
+      lineptr[count++] = p;
+    }
+    return count;
 }
 
 int get_line(char line[], int maxlen) {
@@ -33,3 +32,15 @@ int get_line(char line[], int maxlen) {
   while ((c = getchar()) != '\n' && len < maxlen) line[len++] = c;
   return len;
 }
+
+/*
+   read all the lines of input
+    -> read the line
+    -> make sure that we do not exceed number of lines
+    -> make sure that line does not exceed length limit
+    -> if all passes are correct, allocate space in memory
+    -> ppoint to allocated memory in pointer array
+    -> return -1 if error
+   sort them
+   print them in order
+ */
