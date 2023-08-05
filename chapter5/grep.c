3b29e343cd6271a94457fe750a8a9c2a67af288e
Sangram Barge <sangrambarge098@gmail.com>
2023-08-05 09:18:30 +0530

    grep with optional flags

--- chapter5/grep.c
+++ chapter5/grep.c
@@ -3,12 +3,37 @@
 #include "lib/pattern.c"
 
 #define MAXLINE 1000
+#define OPTFLAG '-'
+#define EFLAG 'x'
+#define NFLAG 'n'
 
 int main(int argc, char *argv[]) {
-  char line[MAXLINE];
-  if (argc != 2) printf("usage: find pattern\n");
+  char line[MAXLINE], *s;
+  int lineflag = 0, exceptflag = 0, linecount = 0;
+
+  while(--argc > 0 && (*++argv)[0] == OPTFLAG)
+    for (s = argv[0] + 1; *s != EOFF; s++)
+      switch(*s) {
+        case EFLAG:
+          exceptflag = 1;
+          break;
+        case NFLAG:
+          lineflag = 1;
+          break;
+        default:
+          printf("invalid input\n");
+          argc = 0;
+          break;
+      }
+  printf("test flags except flag %d number flag %d\n", exceptflag, lineflag);
+  if(argc != 1) printf("Usage: find -x -n patter \n");
   else
-    while (get_line(line, MAXLINE) > 0)
-      if (idx(line, argv[1]) >= 0)
+    while (get_line(line, MAXLINE) > 0){
+      linecount++;
+      if ((idx(line, *argv) >= 0) != exceptflag){
+        if (linecount)
+          printf("%d ",linecount);
         printf("%s", line);
+      }
+    }
 }
