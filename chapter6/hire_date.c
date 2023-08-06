25da35f36024a650856622772f3a394cfff810a8
Sangram Barge <sangrambarge098@gmail.com>
2023-08-06 11:20:00 +0530

    structs

---
+++ chapter6/hire_date.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct date {
+  int day;
+  int month;
+  int year;
+  int yearday;
+  char month_name[10];
+};
+
+static int day_tab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+void day_of_month(struct date *dp);
+void day_of_year(struct date *dp);
+
+int main(void) {
+  struct date hiredate1;
+  struct date hiredate2;
+  hiredate1.yearday = 218;
+  hiredate1.year = 2023;
+  day_of_month(&hiredate1);
+  printf("hdate1 day : %d, month : %d, year : %d, yday : %d \n", hiredate1.day, hiredate1.month, hiredate1.year, hiredate1.yearday);
+
+  hiredate2.year = 2023;
+  hiredate2.day = 6;
+  hiredate2.month = 8;
+  day_of_year(&hiredate2);
+  printf("hdate2 day : %d, month : %d, year : %d, yday : %d \n", hiredate2.day, hiredate2.month, hiredate2.year, hiredate2.yearday);
+  return 0;
+}
+
+void day_of_month(struct date *dp) {
+  int leap = (dp->year % 4 == 0 && dp -> year % 100 != 0) || dp -> year % 400 == 0;
+  dp -> day = dp -> yearday;
+  for (dp -> month = 1; dp -> day >= day_tab[leap][dp -> month]; dp -> month++)
+    dp -> day -= day_tab[leap][dp->month];
+} 
+
+void day_of_year(struct date *dp) {
+  int leap = (dp->year % 4 == 0 && dp -> year % 100 != 0) || dp -> year % 400 == 0;
+  int i;
+  for (i = 1, dp -> yearday = dp -> day; i < (dp -> month); i++) 
+     dp -> yearday += day_tab[leap][i];
+}
