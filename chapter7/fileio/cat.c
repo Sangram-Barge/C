b74a7043237a8ff6aed44e401f051eda3ffed8d0
Sangram Barge <sangrambarge098@gmail.com>
2023-08-12 21:00:24 +0530

    added source and destinition to fcopy

--- chapter7/fileio/cat.c
+++ chapter7/fileio/cat.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
-void file_copy(FILE *fp);
+void f_copy(FILE *fps, FILE *fpd);
 
 int main(int argc, char *argv[]) {
   FILE *fp;
-  if (argc == 1) file_copy(stdin);
-  else {
-    while (--argc > 0) {
-      if ((fp = fopen(*++argv, "r")) == NULL) {
-        printf("cant open file %s\n", *argv);
-        break;
-      } else {
-        file_copy(fp);
-        fclose(fp);
-      }
+  if (argc == 1) f_copy(stdin, stdout);
+  else 
+    while (--argc > 0 ) 
+    if ((fp = fopen(*++argv, "r")) == NULL) {
+      printf("cat: cant open file %s\n", *argv);
+      break;
+    } else {
+      f_copy(fp, stdout);
+      fclose(fp);
     }
-  }
 }
 
-void file_copy(FILE *fp) {
+void f_copy(FILE *fps, FILE *fpd) {
   int c;
-  while((c = getc(fp)) != EOF) putc(c, stdout);
+  while ((c = getc(fps)) != EOF) putc(c, fpd);
 }
