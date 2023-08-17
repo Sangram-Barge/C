17be27f1b202b4539f86b7086238175c506ca1c4
Sangram Barge <sangrambarge098@gmail.com>
2023-08-17 22:07:54 +0530

    xml crawling through nodes

---
+++ exploring_c_libraries/xml/recursive_parse.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libxml/parser.h>
+
+void error(char *s1, char *s2);
+static void crawl(xmlNodePtr node);
+
+int main(void) {
+  char *fileName = "test.xml";
+  xmlDocPtr doc;
+  xmlNodePtr node;
+  doc = xmlParseFile(fileName);
+  if (doc == NULL) error("unable to open file %s", fileName);
+  if ((node = xmlDocGetRootElement(doc)) == NULL) {
+    error("doc %s is empty", fileName);
+    xmlFreeDoc(doc);
+  }
+  crawl(node);
+  xmlFreeDoc(doc);
+}
+
+void error(char *s1, char *s2) {
+  fprintf(stderr, s1, s2);
+  fprintf(stderr, "\n", NULL);
+  exit(1);
+}
+
+static void crawl(xmlNodePtr node) {
+  xmlNodePtr current;
+  for (current = node; current; current = current -> next) {
+    if (current -> type == XML_ELEMENT_NODE) fprintf(stdout, "%s\n", current -> name);
+    crawl(current -> children);
+  }
+}
