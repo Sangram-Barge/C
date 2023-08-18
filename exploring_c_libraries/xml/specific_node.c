d952a6575f0eea3cd870a46cc1f4ee38fa814e92
Sangram Barge <sangrambarge098@gmail.com>
2023-08-18 08:50:28 +0530

    node parsing

---
+++ exploring_c_libraries/xml/specific_node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+
+void error(char *s1, char* s2);
+int main(void) {
+  char *fileName = "test.xml";
+  xmlDocPtr doc;
+  xmlNodePtr rootNode;
+  if ((doc = xmlParseFile(fileName)) == NULL) error("file %s cannot be opend", fileName);
+  if ((rootNode = xmlDocGetRootElement(doc)) == NULL) error("file %s cannot access root", fileName);
+  rootNode = rootNode -> children;
+  while (rootNode != NULL) {
+    if (!xmlStrcmp(rootNode -> name, (xmlChar*)"notes")) fprintf(stdout, "found node : %s\n", rootNode -> name);
+    rootNode = rootNode -> next;
+  }
+  xmlFreeDoc(doc);
+}
+
+void error(char *s1, char *s2) {
+  fprintf(stderr, s1, s2);
+  fprintf(stderr, "\n", NULL);
+  exit(1);
+}
